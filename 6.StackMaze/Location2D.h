3ff133dc1c6fc29ae5e9c44687447e5df22b24c7
sine <qkrtlsgp0996@gmail.com>
2026-02-19 14:59:21 +0900

    큐 자료구조 구현
    - 템플릿 버전으로 큐 클래스 구현

---
+++ 6.StackMaze/Location2D.h
@@ -0,0 +1,21 @@
+#pragma once
+
+//행렬 좌표를 관리하는 클래스
+class Location2D
+{
+public:
+	//행(세로), y
+	int row = 0;
+	//열(가로), x
+	int col = 0;
+
+public:
+	Location2D(int row = 0, int col = 0)
+		:row(row), col(col)
+	{
+	}
+
+
+private:
+
+};
